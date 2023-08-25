7bdd732499cda785ef3adfdd2f54853d4ed0355a
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-25 10:28:27 -0300

    added print function arguments

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
+# Ask user for their name
 name = input("What's your name? ")
-print("Hello, {}" + name)
+
+# Say hello to user
+
+    # Concatenate strings ("+")
+    # print("Hello, " + name)
+
+    # Print Function
+    # print("Hello,", name)
+
+    # Print Function / Default end="\n"
+    # print("Hello, ")
+    # print(name)
+
+    # Print Function / Changing end="\n"
+    # print("Hello, ", end='')
+    # print(name)
+
+    # Print Function / Changing end and sep
+    # print("Hello,", name, sep=' ')
+
