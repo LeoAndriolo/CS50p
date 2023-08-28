450f020030822a420ce43370a4c02d8287809790
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-28 17:17:23 -0300

    int to float

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
 print(x + y)
+
+# print(int(input("What's x? ")) + int(input("What's y? "))) / Too complicated
