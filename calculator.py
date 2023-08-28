18b2d06482f4cd327aaca3830724b24d01559b4b
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-28 17:21:34 -0300

    added round function

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-print(x + y)
+z = round(x + y)
+
+print(z)
 
 # print(int(input("What's x? ")) + int(input("What's y? "))) / Too complicated
