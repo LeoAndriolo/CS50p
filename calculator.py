386a8eb541dbe9b62f4099d41b3df6e14d347da0
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-28 17:27:43 -0300

    division output formatting

--- calculator.py
+++ calculator.py
@@ -2,9 +2,9 @@
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-z = round(x + y)
+z = round(x / y, 2)
 
-print(f"{z:,}")
+print(f"{z:.2f}")
 
 
 # print(int(input("What's x? ")) + int(input("What's y? "))) / Too complicated
