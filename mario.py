0b6d2c52c8412ed8d7e88996fe18b93a853443e2
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-05 09:26:47 -0300

    last iteraction

--- mario.py
+++ mario.py
@@ -22,20 +22,12 @@ def main():
 #     # 2 Method
 #     print("#\n" * height, end="")
 
-# def print_row(width):
-#     print("?" * width)
+def print_row(width):
+    print("#" * width)
 
 def print_square(size):
-    # for each row in square
     for i in range(size):
+        print_row(size)
 
-        #for each brick in row
-        for j in range(size):
-
-            # print brick
-            print("#", end="")
-
-        # 
-        print()
 
 main()
