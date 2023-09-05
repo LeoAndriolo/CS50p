4f247ad74eb82293946f7ad78781d08f8ebe85b8
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-05 08:47:49 -0300

    dict implementation

--- hogwarts.py
+++ hogwarts.py
@@ -17,8 +17,12 @@
 # houses = ["Gryffindor","Gryffindor","Gryffindor","Slytherin"]
 
 students = {
-    "Herminone":"Gryffindor",
+    "Hermione":"Gryffindor",
     "Harry":"Gryffindor",
     "Ron":"Gryffindor",
     "Draco":"Slytherin"
     }
+
+# print(students["Hermione"])
+for student in students:
+    print(student, students[student], sep=" ")
