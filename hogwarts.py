33c16005af234790bfaf6f07d27be5f33f56f3ba
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-05 08:31:34 -0300

    using len()

---
+++ hogwarts.py
@@ -0,0 +1,13 @@
+# Code to practice lists
+
+# Simple For loop
+# students = ["Herminone", "Harry", "Ron"]
+
+# for student in students:
+#     print(student)
+
+# Using len()
+students = ["Herminone", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(students[i])
