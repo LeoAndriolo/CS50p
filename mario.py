80508113dbfbcb60d907eb85cc3f72ebd775e539
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-05 09:24:30 -0300

    print_square function

--- mario.py
+++ mario.py
@@ -11,7 +11,8 @@
 # Using functions
 def main():
     # print_column(3)
-    print_row(4)
+    # print_row(4)
+    print_square(3)
 
 # def print_column(height):
 #     # 1 Method
@@ -21,8 +22,20 @@ def main():
 #     # 2 Method
 #     print("#\n" * height, end="")
 
-def print_row(width):
-    print("?" * width)
+# def print_row(width):
+#     print("?" * width)
 
+def print_square(size):
+    # for each row in square
+    for i in range(size):
+
+        #for each brick in row
+        for j in range(size):
+
+            # print brick
+            print("#", end="")
+
+        # 
+        print()
 
 main()
