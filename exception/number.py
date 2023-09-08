557567724e15a611d64871bbb90fe9ea55ca997a
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-08 11:32:47 -0300

    get_int refine, return change location

--- exception/number.py
+++ exception/number.py
@@ -43,10 +43,8 @@ def main():
 def get_int():
     while True:
         try:
-            x = int(input("What's x? "))
+            return int(input("What's x? "))
         except ValueError:
             print("x is not an integer")
-        else:
-            return x # Breaks loop and returns a value
 
 main()
