eab895cd15b2fe59ba3dfb60f0a174ba4f2bdbd6
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-08 11:25:39 -0300

    get_int function

--- exception/number.py
+++ exception/number.py
@@ -1,12 +1,48 @@
 # Exceptions class notes
 
+# P01
 # x = int(input("What's x? "))
 # print(f"x is {x}")
 
 # cat returs ValueError: invalid literal for int() with base 10: 'cat'
 
-try:
-    x = int(input("What's x? "))
-    print(f"x is {x}")
-except ValueError:
-    print("x is not an integer")
+# P02
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("x is not an integer")
+
+# print(f"x is {x}") # Returns NameError for "cat" -> NameError: name 'x' is not defined
+#                    # Caused by int() not assigning a value to x
+
+# P03
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("x is not an integer")
+# else:
+#     print(f"x is {x}")
+    
+# P04
+# while True:
+#     try:
+#         x = int(input("What's x? "))
+#     except ValueError:
+#         print("x is not an integer")
+#     else:
+#         break
+        
+# print(f"x is {x}")
+
+# P05
+def get_int():
+
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
