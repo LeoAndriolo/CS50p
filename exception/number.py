0933ae1922a2440e342058f8ed48dd5fcca13d8e
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-08 11:04:41 -0300

    try and except implementation

--- exception/number.py
+++ exception/number.py
@@ -1,6 +1,12 @@
 # Exceptions class notes
 
-x = int(input("What's x? "))
+# x = int(input("What's x? "))
+# print(f"x is {x}")
 
-print(f"x is {x}")
+# cat returs ValueError: invalid literal for int() with base 10: 'cat'
 
+try:
+    x = int(input("What's x? "))
+    print(f"x is {x}")
+except ValueError:
+    print("x is not an integer")
