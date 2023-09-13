138c755c3282d4949ea8252c0908bc866730e5e8
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-13 10:20:29 -0300

    file update

---
+++ marioException.py
@@ -0,0 +1,25 @@
+# Program to practice debbuging
+
+# Using functions
+def main():
+    # print_column(3)
+    # print_row(4)
+    print_square(3)
+
+# def print_column(height):
+#     # 1 Method
+#     # for _ in range(height):
+#     #     print("#")
+
+#     # 2 Method
+#     print("#\n" * height, end="")
+
+def print_row(width):
+    print("#" * width)
+
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+
+
+main()
