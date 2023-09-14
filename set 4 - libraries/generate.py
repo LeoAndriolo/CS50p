a14347f706790dfc1476cbb016426b683312ce40
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 08:54:47 -0300

    Change import to get only "choice" function

--- set 4 - libraries/generate.py
+++ set 4 - libraries/generate.py
@@ -1,6 +1,7 @@
 # Set 4 Class - Libraries
 
-import random
+#import random -> Imports all functions of "random" library
+from random import choice # Imports only "choice" function
 
 coin = random.choice(["heads","tails"])
 print(coin)
