74f9cbb49c44b5f48bad7bac034930b2e80721bd
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 08:59:22 -0300

    Add Randint and Shuffle functions

--- set 4 - libraries/generate.py
+++ set 4 - libraries/generate.py
@@ -1,7 +1,18 @@
 # Set 4 Class - Libraries
 
-#import random -> Imports all functions of "random" library
-from random import choice # Imports only "choice" function
+import random # Imports all functions of "random" library
+#from random import choice # Imports only "choice" function
 
-coin = random.choice(["heads","tails"])
-print(coin)
+# Hardcoded coin toss
+# coin = random.choice(["heads","tails"])
+# print(coin)
+
+# Randint function
+# number = random.randint(1,10)
+# print(number)
+
+# Shuffle function
+cards = ["jack","queen","king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
