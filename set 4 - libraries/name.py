f7a3f905c5f0a7ba3f791ecb956b61f86223fdb8
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 21:11:01 -0300

    Change sys.argv logic

--- set 4 - libraries/name.py
+++ set 4 - libraries/name.py
@@ -3,7 +3,10 @@
 import sys
 
 # sys.argv # Argument value
-try:
+if len(sys.argv) < 2:
+    print("Too few arguments")    
+elif len(sys.argv) > 2:
+    print("Too many arguments")    
+else:
     print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments")
+    
