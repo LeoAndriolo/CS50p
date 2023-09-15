509f2ee997d2a023c81e93af2516bac933832002
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 21:26:56 -0300

    Correct for loop out of range

--- set 4 - libraries/name.py
+++ set 4 - libraries/name.py
@@ -5,9 +5,8 @@ import sys # sys.argv[0] -> Program name
 # sys.argv # Argument value
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")    
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")    
 
-print("Hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)
     
     
