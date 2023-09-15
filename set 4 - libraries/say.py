4a1e4a1c912faf3cf15bce8ace066f32232967fb
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 22:02:00 -0300

    Change cowsay to custom lib "sayings"

--- set 4 - libraries/say.py
+++ set 4 - libraries/say.py
@@ -1,8 +1,14 @@
-# Set 4 Class - Libraries - Packages - Cowsay
+# Set 4 Class - Libraries - Packages - Cowsay + Custom sayings lib
 
-import cowsay
+# import cowsay
+# import sys
+
+# if len(sys.argv) == 2:
+#     cowsay.cow("Hello, " + sys.argv[1])
+#     # cowsay.trex("Hello, " + sys.argv[1])
+
 import sys
+from sayings import hello
 
 if len(sys.argv) == 2:
-    cowsay.cow("Hello, " + sys.argv[1])
-    # cowsay.trex("Hello, " + sys.argv[1])
+    hello(sys.argv[1])
