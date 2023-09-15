a6247db1ac1df665df98b2dc8b0e5fa2bdc85c1f
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-14 21:42:59 -0300

    Create file

---
+++ set 4 - libraries/itunes.py
@@ -0,0 +1,10 @@
+# Set 4 Class - Libraries - API
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json())
