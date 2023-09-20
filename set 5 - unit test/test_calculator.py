b6c8a205ac4be77530f2ac76061c0df78a34cc89
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-20 17:58:54 -0300

    Add more assert cases

--- set 5 - unit test/test_calculator.py
+++ set 5 - unit test/test_calculator.py
@@ -19,6 +19,18 @@ def test_square():
         assert square(3) == 9
     except AssertionError:
         print("3 square was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 square was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 square was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 square was not 0")
 
 
 if __name__ == "__main__":
