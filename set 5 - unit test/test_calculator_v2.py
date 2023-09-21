498e4f997eb45e17bc17503e6e2a9e0e213c5a9d
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-20 21:44:04 -0300

    Revert the bug to test

--- set 5 - unit test/test_calculator_v2.py
+++ set 5 - unit test/test_calculator_v2.py
@@ -2,15 +2,13 @@
 
 from calculator import square
 
-def main():
-    test_square()
-
-def test_square(): # Run "pytest" to check code
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
-    assert square(0) == 0
 
-if __name__ == "__main__":
-    main()
+def test_zero():
+    assert square(0) == 0
