3e1693edfdcef0624480365b327ea52ef0b7e457
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-22 08:08:29 -0300

    Separate default from argument asserts

--- set 5 - unit test/test_hello.py
+++ set 5 - unit test/test_hello.py
@@ -2,5 +2,8 @@
 
 from hello import hello
 
-def test_hello():
-    hello("David") == "hello, David"
+def test_default():
+    assert hello() == "hello, world"
+
+def test_argument():
+    assert hello("David") == f"hello, David"
