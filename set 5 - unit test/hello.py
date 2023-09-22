37a62628c764d2e534abe017a3d0fbd2e51f818e
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-22 08:03:47 -0300

    Update hello files

---
+++ set 5 - unit test/hello.py
@@ -0,0 +1,12 @@
+# Set 5 Class - Unit Test - Test Hello
+
+def main():
+    name = input("What's your name? ")
+    print(hello(name))
+
+def hello(to="world"):
+    # print("hello,",to)
+    return f"hello, {to}"
+
+if __name__ == "__main__":
+    main()
---
+++ set 5 - unit test/test_hello.py
@@ -0,0 +1,6 @@
+# Set 5 Class - Unit Test - Test Hello
+
+from hello import hello
+
+def test_hello():
+    hello("David") == "hello, David"
