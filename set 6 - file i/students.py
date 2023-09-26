1041a81dbbbb944eb4041e19f8cd191f9c12a071
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-26 15:54:06 -0300

    Add row by row methods

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -1,6 +1,13 @@
 # Set 6 Class - File I/O - Students
 
-with open("students.csv") as file:
-    for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+# Row by Row - List
+    # with open("students.csv") as file:
+    #     for line in file:
+    #         row = line.rstrip().split(",")
+    #         print(f"{row[0]} is in {row[1]}")
+
+# Row by Row - Dict
+    # with open("students.csv") as file:
+    #     for line in file:
+    #         name, house = line.rstrip().split(",")
+    #         print(f"{name} is in {house}")
