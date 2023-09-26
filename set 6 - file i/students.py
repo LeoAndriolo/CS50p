76cd83974c0f7d39b8bcf9750926843f8001505d
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-26 15:56:12 -0300

    Add sorted method

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -11,3 +11,15 @@
     #     for line in file:
     #         name, house = line.rstrip().split(",")
     #         print(f"{name} is in {house}")
+
+# Row by Row - Sorted
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print(student)
+
