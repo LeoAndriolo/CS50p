183506a5023ad1dcbed13d4d53fadaa35a7a4e6d
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-27 10:40:03 -0300

    Implement DictReader syntax

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -33,11 +33,9 @@ import csv
 students = []
 
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    # for row in reader:
-    #     students.append({"name": row[0], "home": row[1]}) -> Row syntax
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
