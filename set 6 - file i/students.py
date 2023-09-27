9a32db31d6828958ce0399e1ef1c1b9455132d3e
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-27 10:59:44 -0300

    Implement CSV Read

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -28,15 +28,16 @@
     #     print(f"{student['name']} is in {student['house']}")
 
 # csv Function
-import csv
+    # Reading
+        # import csv
 
-students = []
+        # students = []
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
+        # with open("students.csv") as file:
+        #     reader = csv.DictReader(file)
+        #     for row in reader:
+        #         students.append({"name": row["name"], "home": row["home"]})
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+        # for student in sorted(students, key=lambda student: student["name"]):
+        #     print(f"{student['name']} is from {student['home']}")
 
