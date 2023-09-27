87cbdd6c415ce409872a3ea72310104eb91adedd
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-27 09:14:37 -0300

    Comment sorted and lambda

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -12,18 +12,18 @@
     #         name, house = line.rstrip().split(",")
     #         print(f"{name} is in {house}")
 
-# Row by Row - Sorted Dict
-students = []
+# Row by Row - Sorted Dict - Lambda
+    # students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    # with open("students.csv") as file:
+    #     for line in file:
+    #         name, house = line.rstrip().split(",")
+    #         student = {"name": name, "house": house}
+    #         students.append(student)
 
-# def get_name(student):
-#     return student["name"]
+    # # def get_name(student):
+    # #     return student["name"]
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    # for student in sorted(students, key=lambda student: student["name"]):
+    #     print(f"{student['name']} is in {student['house']}")
 
