5f2e5634e71cc21c8298101460feeda19d58908f
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-09-27 10:38:06 -0300

    Implement csv syntax

--- set 6 - file i/students.py
+++ set 6 - file i/students.py
@@ -27,3 +27,18 @@
     # for student in sorted(students, key=lambda student: student["name"]):
     #     print(f"{student['name']} is in {student['house']}")
 
+# csv Function
+import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    # for row in reader:
+    #     students.append({"name": row[0], "home": row[1]}) -> Row syntax
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
