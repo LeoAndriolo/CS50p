281773b6beaa68b438d35bbc1e8c4d135ef60f00
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-02 10:50:21 -0300

    Split username and domain from email

--- set 7 - regular expressions/validate.py
+++ set 7 - regular expressions/validate.py
@@ -2,7 +2,16 @@
 
 email = input("What's your email? ").strip()
 
-if "@" in email and "." in email:
+# Condition validation
+    # if "@" in email and "." in email:
+    #     print("Valid")
+    # else:
+    #     print("Invalid")
+
+# Using str methods
+username, domain = email.split("@")
+
+if username and domain.endswith(".edu"):
     print("Valid")
 else:
     print("Invalid")
