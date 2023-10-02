99a45c483514699170349398931362dee1d1f003
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-02 17:04:23 -0300

    Update search to be case insensitive

--- set 7 - regular expressions/validate.py
+++ set 7 - regular expressions/validate.py
@@ -19,7 +19,7 @@ email = input("What's your email? ").strip()
     #     print("Invalid")
 
 # Regular expressions (re) library
-if re.search(r"^\w+@\w+\.edu$",email): # [a-zA-Z0-9_] alpha + numeric + _ | \w -> Any word character like [a-zA-Z0-9_]
+if re.search(r"^\w+@\w+\.edu$",email, re.IGNORECASE): # [a-zA-Z0-9_] alpha + numeric + _ | \w -> Any word character like [a-zA-Z0-9_]
     print("Valid")
 else:
     print("Invalid")
