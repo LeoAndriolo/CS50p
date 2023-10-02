281397cf5a0f3b4a394efed62f6a9d3557ad5221
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-02 10:53:50 -0300

    Implement re library

--- set 7 - regular expressions/validate.py
+++ set 7 - regular expressions/validate.py
@@ -1,5 +1,7 @@
 # Set 7 Class - Regular Expressions
 
+import re
+
 email = input("What's your email? ").strip()
 
 # Condition validation
@@ -9,9 +11,15 @@ email = input("What's your email? ").strip()
     #     print("Invalid")
 
 # Using str methods
-username, domain = email.split("@")
+    # username, domain = email.split("@")
 
-if username and domain.endswith(".edu"):
+    # if username and domain.endswith(".edu"):
+    #     print("Valid")
+    # else:
+    #     print("Invalid")
+
+# Regular expressions (re) library
+if re.search("@",email):
     print("Valid")
 else:
     print("Invalid")
