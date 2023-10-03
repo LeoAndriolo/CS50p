5afa698ea0900cd16b1844bccdd6d855960d7682
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-03 08:53:20 -0300

    Update regular expression from username

--- set 7 - regular expressions/twitter.py
+++ set 7 - regular expressions/twitter.py
@@ -1,7 +1,16 @@
 # Set 7 Class - Regular Expressions
 
+# Intro to get url
+    # url = input("URL: ").strip()
+
+    # # username = url.replace("https://twitter.com/","")
+    # username = url.removeprefix("https://twitter.com/")
+    # print(f"Username: {username}")
+
+# Using re library
+import re
+
 url = input("URL: ").strip()
 
-# username = url.replace("https://twitter.com/","")
-username = url.removeprefix("https://twitter.com/")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 print(f"Username: {username}")
