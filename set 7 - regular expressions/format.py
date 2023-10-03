71e7805712befe669e30b5d0bf1e8625e276d7db
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-03 08:22:58 -0300

    Split name with groups method

---
+++ set 7 - regular expressions/format.py
@@ -0,0 +1,20 @@
+# Set 7 Class - Regular Expressions
+
+# First interaction
+    # name = input("What's your name? ").strip()
+
+    # if "," in name:
+    #     first, last = name.split(", ")
+    #     name = f"{first} {last}"
+
+    # print(f"hello, {name}")
+    
+# re lib
+import re
+
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last, first = matches.groups()
+    name = f"{first} {last}"
+print(f"hello, {name}")
