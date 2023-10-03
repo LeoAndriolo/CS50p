a7d6fb01ee1f05fb171e629906dd478068ef9967
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-03 08:32:22 -0300

    Change condition to walrus syntax

--- set 7 - regular expressions/format.py
+++ set 7 - regular expressions/format.py
@@ -12,10 +12,23 @@
 # re lib
 import re
 
+# Groups
+    # name = input("What's your name? ").strip()
+    # matches = re.search(r"^(.+), *(.+)$", name)
+    # if matches:
+    #     last, first = matches.groups()
+    #     name = f"{first} {last}"
+    # print(f"hello, {name}")
+
+# Separated group
+    # name = input("What's your name? ").strip()
+    # matches = re.search(r"^(.+), *(.+)$", name)
+    # if matches:
+    #     name = matches.group(2) + " " + matches.group(1)
+    # print(f"hello, {name}")
+
+# Walrus syntax
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), *(.+)$", name)
-if matches:
-    # last, first = matches.groups()
-    # name = f"{first} {last}"
+if matches := re.search(r"^(.+), *(.+)$", name):
     name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
