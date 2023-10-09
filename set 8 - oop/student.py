b1926a46061b1a24481d4535316af27963694c08
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 16:10:20 -0300

    Implement get and set methods for house

--- set 8 - oop/student.py
+++ set 8 - oop/student.py
@@ -17,44 +17,74 @@
     # if __name__ == "__main__":
     #     main()
 
-# OOP
+# OOP (init, str, custom method)
+    # class Student:
+    #     def __init__(self, name, house, patronus):   # Initializes content of a method (function inside a class)
+    #         if not name:
+    #             raise ValueError("Missing name")
+    #         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+    #             raise ValueError("Invalid house")
+    #         self.name = name
+    #         self.house = house
+    #         self.patronus = patronus
+    #     def __str__(self):
+    #         return f"{self.name} from {self.house}" 
+    #     def charm(self):
+    #         match self.patronus:
+    #             case "Stag":
+    #                 return "🦌"
+    #             case "Otter":
+    #                 return "🦦"
+    #             case "Jack Russel Terrier":
+    #                 return "🐕"
+    #             case _:
+    #                 return "🤨"
+    # def main():
+    #     student = get_student()
+    #     print("Expecto Patronum!")
+    #     print(student.charm())
+    # def get_student():
+    #     name = input("Name: ")
+    #     house = input("House: ")
+    #     patronus = input("Patronus: ")
+    #     return Student(name, house, patronus)   # Constructor call
+    # if __name__ == "__main__":
+    #     main()
 
 class Student:
-    def __init__(self, name, house, patronus):   # Initializes content of a method (function inside a class)
+    def __init__(self, name, house):   # Initializes content of a method (function inside a class)
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+        # if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+        #     raise ValueError("Invalid house")
         self.name = name
         self.house = house
-        self.patronus = patronus
-
+    
     def __str__(self):
         return f"{self.name} from {self.house}"
     
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "🦌"
-            case "Otter":
-                return "🦦"
-            case "Jack Russel Terrier":
-                return "🐕"
-            case _:
-                return "🤨"
-            
+    @property   # Getter
+    def house(self):
+        return self._house   # _house -> Instance variable
+    
+    @house.setter   # Setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house   # _house -> Instance variable
+    
 
+    
 def main():
     student = get_student()
-    print("Expecto Patronum!")
-    print(student.charm())
+
+    print(student)
     
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)   # Constructor call
+    return Student(name, house)   # Constructor call
 
 if __name__ == "__main__":
     main()
