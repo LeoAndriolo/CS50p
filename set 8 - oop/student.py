120d8eb0012ff2a357a01d3d491c104616cbab54
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 08:26:09 -0300

    Change return from list to dict

--- set 8 - oop/student.py
+++ set 8 - oop/student.py
@@ -1,17 +1,34 @@
 # Set 8 Class - Object-Oriented Programming
 
+# Intro logic
+    # def main():
+    #     student = get_student()   # Assign Tuple that contains 2 elements per index
+    #     if student[0] == "Padma":
+    #         student[1] = "Ravenclaw"
+    #     print(f"{student[0]} from {student[1]}")
+        
+
+    # def get_student():
+    #     name = input("Name: ")
+    #     house = input("House: ")
+    #     # return (name, house)   # Tuple does NOT support item assigment
+    #     return [name, house]   # Lists support item assigment
+
+    # if __name__ == "__main__":
+    #     main()
+
+# OOP
 def main():
-    student = get_student()   # Assign Tuple that contains 2 elements per index
-    if student[0] == "Padma":
-        student[1] = "Ravenclaw"
-    print(f"{student[0]} from {student[1]}")
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
     
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    # return (name, house)   # Tuple does NOT support item assigment
-    return [name, house]   # Lists support item assigment
+    return {"name": name,"house": house}   # Dicts support item assigment
 
 if __name__ == "__main__":
     main()
