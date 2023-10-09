f40d64c99862528a8b620e02e5b680b767f8b2bc
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 07:53:17 -0300

    Correct if statment on matches

--- set 7 - regular expressions/twitter.py
+++ set 7 - regular expressions/twitter.py
@@ -13,5 +13,5 @@ import re
 url = input("URL: ").strip()
 
 # username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
-matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE): # ?: -> Non-capturing this () syntax
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE): # ?: -> Non-capturing this () syntax
     print(f"Username: {matches.group(1)}")
