82d7710eccd2c016ebe6d3a27c38231eff9ffc3c
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 16:30:10 -0300

    Create file

---
+++ set 8 - oop/type.py
@@ -0,0 +1,8 @@
+# Set 8 Class - Types of Python
+
+print(type(50))     # int
+print(type("50"))   # str
+print(type([]))     # list
+print(type(list())) # list
+print(type({}))     # dict
+print(type(dict())) # dict
