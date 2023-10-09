9f07493e55e43a365546c9002756e6aecb4b082b
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 08:08:10 -0300

    Implement Tuple for student

--- set 8 - oop/student.py
+++ set 8 - oop/student.py
@@ -1,13 +1,13 @@
 # Set 8 Class - Object-Oriented Programming
 
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()   # Assign Tuple that contains 2 elements per index
+    print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name = input("Name: ")
-    house input("House: ")
-    return name, house
+    house = input("House: ")
+    return (name, house)   # Tuple
 
 if __name__ == "__main__":
     main()
