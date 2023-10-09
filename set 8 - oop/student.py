16362f4da1efa96c86acb8847be4271a2a0c9b3f
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-09 08:12:24 -0300

    Change get_student return from tuple to list

--- set 8 - oop/student.py
+++ set 8 - oop/student.py
@@ -2,12 +2,16 @@
 
 def main():
     student = get_student()   # Assign Tuple that contains 2 elements per index
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
     print(f"{student[0]} from {student[1]}")
+    
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return (name, house)   # Tuple
+    # return (name, house)   # Tuple does NOT support item assigment
+    return [name, house]   # Lists support item assigment
 
 if __name__ == "__main__":
     main()
