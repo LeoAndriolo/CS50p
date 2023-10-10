563c3463e3d2d481482102aa7687dd5c9cb85f74
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-10-10 08:57:24 -0300

    Add Wizard class

--- set 8 - oop/wizard.py
+++ set 8 - oop/wizard.py
@@ -1,19 +1,21 @@
 # Set 8 Class - Object-Oriented Programming - Inheritance
 
-class Student:
-    def __init__(self, name, house):
+class Wizard:
+    def __init__(self, name):
         if not name:
             raise ValueError("Missing name")
         self.name = name
+    
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
     ...
 
 
-class Professor:
+class Professor(Wizard):
     def __init__(self, name, subject):
-        if not name:
-            raise ValueError("Missing name")
-        self.name = name
+        super().__init__(name)
         self.subject = subject
  
